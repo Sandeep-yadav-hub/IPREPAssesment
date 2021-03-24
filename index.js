a5e33d6074cb45f80b8cb87be956da0b6b7ee9eb
pkyad <pkyisky@gmail.com>
2021-03-24 22:17:49 +0530

    fisrt commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function toDataURL(src, callback) {
+  var img = new Image();
+  img.onload = function () {
+    var canvas = document.createElement("CANVAS");
+    var ctx = canvas.getContext("2d");
+    var dataURL;
+    canvas.height = this.naturalHeight;
+    canvas.width = this.naturalWidth;
+    ctx.drawImage(this, 0, 0);
+    dataURL = canvas.toDataURL();
+    callback(dataURL);
+  };
+  img.src = src;
+}
+
+var img = document.getElementById("img")
+var src = "img.jpeg"
+toDataURL(
+  `${window.location.origin}/${src}`,
+  function (dataUrl) {
+    img.src = dataUrl;
+  }
+);
+
+
